80091c4fc181adaa248b78c4a5a6aac3e9274d0e
Jerzy Adamowski <jerzy.adamowski@ringieraxelspringer.pl>
2021-02-19 19:00:03 +0100

    from attr to scoping rules

---
+++ 13_attributes/03_01_custom.rs
@@ -0,0 +1,11 @@
+#[cfg(some_condition)]
+fn conditional_function() {
+    println!("condition met!");
+}
+
+fn main() {
+    conditional_function();
+}
+
+// $ rustc --cfg some_condition 03_01_custom.rs && ./03_01_custom
+// condition met!
